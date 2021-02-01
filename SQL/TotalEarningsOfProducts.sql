db260d70cc3fbb16bdcd6d953108d0af99c6dba7
mcaliskan7 <merve.caliskan@std.yeditepe.edu.tr>
2021-02-01 18:40:34 +0300

    February 1, 2021

---
+++ SQL/TotalEarningsOfProducts.sql
@@ -0,0 +1,11 @@
+﻿--joins the tables 'Products', 'Orders' and 'Order Details'
+--calculates how much money earned in total for each product
+--sorts in reverse first by total earnings then by product name
+
+select ProductName as [Product Name],
+sum([Order Details].UnitPrice * [Order Details].Quantity) as [Total Earnings]
+from [Order Details]
+inner join Products on Products.ProductID = [Order Details].ProductID
+inner join Orders on Orders.OrderID = [Order Details].OrderID
+group by ProductName
+order by [Total Earnings], [Product Name] desc
